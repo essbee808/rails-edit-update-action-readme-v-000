f324d70689daa02b9110d4e03f2c5af8367d3c10
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-03 05:39:14 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,6 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(title: params[:article][:title], params[:article][:description])
     redirect_to article_path(@article)
-    
   end
 
 end
